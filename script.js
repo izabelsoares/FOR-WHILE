80a52f378f68867fb4b7bb47d398e6ae5ccd8ab4
izabelsoares <izabelsoaresbds@gmail.com>
2021-05-14 21:03:52 -0300

    terceiro exercicio de logica

---
+++ script.js
@@ -0,0 +1,35 @@
+//
+
+for (let y = 1; y <= 10; y++) {
+    console.log(y)
+} 
+
+//
+
+//
+
+ let z = 10;
+
+while (z >= 1){
+    console.log(z)
+    z--
+}
+
+//
+
+//
+
+for (let x = 1; x <= 100; x+=2 ) {
+    console.log(x)
+}
+
+//
+
+let w = 0;
+
+while (w <= 100){
+    console.log(w)
+    w+=2
+}
+
+//
